3f4f13ec62d784491d8d38b40040bbcd5d34338c
Artem Perminov <perminov.artem@eurostavka.ru>
2022-03-08 02:25:12 +0300

    refactoring

--- src/cli.js
+++ src/cli.js
@@ -2,7 +2,7 @@ import readlineSync from 'readline-sync';
 
 const userNameRequest = () => {
   const userName = readlineSync.question('May I have your name? ');
-  console.log(`Hi my friend, ${userName}!`);
+  console.log(`Hello, ${userName}!`);
 };
 export {
   userNameRequest
