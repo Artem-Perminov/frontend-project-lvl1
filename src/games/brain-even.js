eb3824c0d2d8a5fc25ab4e78b1d41b0afa8a6817
Artem Perminov <perminov.artem@eurostavka.ru>
2022-03-13 21:16:48 +0300

    refactoring

--- src/games/brain-even.js
+++ src/games/brain-even.js
@@ -2,12 +2,15 @@ import { roundsCount, engine } from '../index.js';
 import getRandomNumber from '../helpers.js';
 
 const gameDescription = 'Answer "yes" if the number is even, otherwise answer "no".';
+
 const parityChecking = (num) => num % 2 === 0;
+
 const generateRound = () => {
   const question = getRandomNumber(1, 100);
   const correctAnswer = parityChecking(question) ? 'yes' : 'no';
   return [question, correctAnswer];
 };
+
 const brainEven = () => {
   const rounds = [];
   for (let i = 0; i < roundsCount; i += 1) {
--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -2,6 +2,7 @@ import { roundsCount, engine } from '../index.js';
 import getRandomNumber from '../helpers.js';
 
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
 const checkNumberPrime = (num) => {
   for (let i = 2; i < num - 1; i += 1) {
     if (num === 0) {
@@ -13,11 +14,13 @@ const checkNumberPrime = (num) => {
   }
   return true;
 };
+
 const generateRound = () => {
   const question = getRandomNumber(1, 100);
   const correctAnswer = checkNumberPrime(question) === true ? 'yes' : 'no';
   return [question, correctAnswer];
 };
+
 const brainPrime = () => {
   const rounds = [];
   for (let i = 0; i < roundsCount; i += 1) {
