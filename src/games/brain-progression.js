868f2ec7ec5684ad58a6603b80b4290d8f62be87
Artem Perminov <perminov.artem@eurostavka.ru>
2022-03-16 20:18:54 +0300

    refactoring

--- src/games/brain-progression.js
+++ src/games/brain-progression.js
@@ -3,34 +3,25 @@ import getRandomNumber from '../helpers.js';
 
 const gameDescription = 'What number is missing in the progression?';
 
-const getProgression = (progCoef) => {
+const getProgression = (progStep, firstNum, progLength) => {
   const progression = [];
-  let firstRanNum = getRandomNumber(1, 100);
-  const cycleLength = 10;
-  for (let i = 0; i < cycleLength; i += 1) {
-    progression.push(firstRanNum + progCoef);
-    firstRanNum = progression[progression.length - 1];
+  let value = firstNum;
+  for (let i = 0; i < progLength; i += 1) {
+    progression.push(value + progStep);
+    value = progression[progression.length - 1];
   }
-  const posHiddenElement = Math.floor(Math.random() * 10);
-  progression[posHiddenElement] = '..';
-  return progression.join(' ');
-};
-
-const getProgressionNum = (progr, progCoef) => {
-  let result;
-  const valuesArr = progr.split(' ');
-  if (valuesArr.indexOf('..') === 0) {
-    result = Number(valuesArr[1]) - progCoef;
-  } else {
-    result = Number(valuesArr[valuesArr.indexOf('..') - 1]) + progCoef;
-  }
-  return result;
+  return progression;
 };
 
 const generateRound = () => {
-  const progCoeff = getRandomNumber(1, 10);
-  const question = getProgression(progCoeff);
-  const correctAnswer = String(getProgressionNum(question, progCoeff));
+  const progStep = getRandomNumber(1, 10);
+  const firstNum = getRandomNumber(1, 100);
+  const progLength = 10;
+  const progression = getProgression(progStep, firstNum, progLength);
+  const hiddenIndex = getRandomNumber(1, progLength);
+  const correctAnswer = String(progression[hiddenIndex]);
+  progression[hiddenIndex] = '..';
+  const question = progression.join(' ');
   return [question, correctAnswer];
 };
 
