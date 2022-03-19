c1f046c77b2534ffc1cc00c1b1145f7b02959441
Artem Perminov <perminov.artem@eurostavka.ru>
2022-03-19 15:55:23 +0300

    refactoring

--- src/games/brain-prime.js
+++ src/games/brain-prime.js
@@ -4,10 +4,10 @@ import getRandomNumber from '../helpers.js';
 const gameDescription = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const checkNumberPrime = (num) => {
+  if (num < 2) {
+    return false;
+  }
   for (let i = 2; i < num - 1; i += 1) {
-    if (num < 2) {
-      return false;
-    }
     if (num % i === 0) {
       return false;
     }
